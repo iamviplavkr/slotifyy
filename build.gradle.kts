f45ba828941d72b340415d93498347d13efefba9
Viplav Kumar <viplav1998@gmail.com>
2026-02-14 17:59:05 +0530

    Initial Slotify Android Commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,5 @@
-// Root-level build.gradle.kts
-
 plugins {
-    // Define versions here ONCE for the whole project
-    id("com.android.application") version "8.10.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-}
-
-task<Delete>("clean") {
-    delete(rootProject.buildDir)
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
